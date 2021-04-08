d5452702d92a5c60ce03afb1e7e6c28c2472a091
nszczerba99 <nataliaszczerba00@gmail.com>
2021-04-08 04:05:52 +0200

    added request class

--- js/rest.js
+++ js/rest.js
@@ -1,15 +1,124 @@
-var appAddress = "https://io-projekt-gamepage-api.herokuapp.com";
+// var appAddress = "https://io-projekt-gamepage-api.herokuapp.com";
+var appAddress = "http://localhost:5000";
 
-function getRequestObject() {
-  if (window.ActiveXObject)
-    return(new ActiveXObject("Microsoft.XMLHTTP"));
+//class representing ajax requests
+class Request {
+  constructor(type, url, data, onSuccess, headers=null) {
+    this.type = type;
+    this.url = url;
+    this.data = data;
+    this.onSuccess = onSuccess;
+    this.headers = headers;
+  }
 
-  else if (window.XMLHttpRequest)
-    return (new XMLHttpRequest());
+  send() {
+    let request = Request.getReqObject();
+    request.onreadystatechange = () => { this.responseHandler(request) };
+    request.open(this.type, this.url, true);
 
-  else
-    return(null);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    for (const key in this.headers)
+      request.setRequestHeader(key, this.headers[key]);
+
+    request.send(this.data);
+  }
+
+  responseHandler(request) {
+    if (request.readyState == 4) {
+      if (request.status == 200)
+        this.onSuccess(request);
+
+      else
+        Request.onError(request);
+    }
+  }
+
+  static getReqObject() {
+    if (window.ActiveXObject)
+      return (new ActiveXObject("Microsoft.XMLHTTP"));
+
+    else if (window.XMLHttpRequest)
+      return (new XMLHttpRequest());
+
+    else
+      return(null);
+  }
+
+  static onError(request) {
+    alert(`Error ${request.status}: ${request.response}`);
+  }
 }
+
+
+class RequestWithAuth extends Request {
+
+  constructor(type, url, data, onSuccess, headers={'Authorization': 'Bearer ' + getAccessToken()}) {
+    super(type, url, data, onSuccess, headers);
+  }
+
+  responseHandler(request) {
+
+    if (request.readyState == 4) {
+      if (request.status == 200)
+        this.onSuccess(request);
+
+      else if (request.status == 401) {
+        let nextRequest = Request.getReqObject();
+
+        nextRequest.onreadystatechange = () => { this.refreshResponseHandler(nextRequest); };
+        nextRequest.open("POST", `${appAddress}/refresh`, true);
+        nextRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        nextRequest.setRequestHeader('Authorization', 'Bearer ' + getRefreshToken())
+        nextRequest.send(null);
+      }
+
+      else
+        Request.onError(request);
+    }
+  }
+
+  refreshResponseHandler(request) {
+
+      if (request.readyState == 4) {
+        if (request.status == 200) {
+          console.log('refresh');
+
+          let response = JSON.parse(request.response);
+          setAccessToken(response.access_token);
+
+          let nextRequest = Request.getReqObject();
+
+          nextRequest.onreadystatechange = () => { this.authorizedResponseHandler(nextRequest); };
+          nextRequest.open(this.type, this.url, true);
+          nextRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+          nextRequest.setRequestHeader('Authorization', 'Bearer ' + getAccessToken())
+          nextRequest.send(this.data);
+        }
+
+        else if (request.status == 401)
+          onSessionLost();
+
+        else
+          Request.onError(request);
+      }
+  }
+
+  authorizedResponseHandler(request) {
+
+    if (request.readyState == 4) {
+      if(request.status == 200)
+        this.onSuccess(request);
+
+      else if (request.status == 401)
+        onSessionLost();
+
+      else
+        Request.onError(request);
+    }
+  }
+}
+
+//ajax functions
 
 function signupme() {
   let form = document.getElementsByTagName("form")[0];
@@ -18,21 +127,7 @@ function signupme() {
     'password': form.pass.value
   };
 
-  let request = getRequestObject();
-  request.onreadystatechange = function() {
-    if (request.readyState == 4) {
-      if(request.status == 200) {
-        loginme(user);
-      }
-
-      else {
-        alert(`Error ${request.status}: ${request.response}`);
-      }
-    }
-  }
-  request.open("POST", `${appAddress}/api/add/user`, true);
-  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  request.send(JSON.stringify(user));
+  new Request("POST", `${appAddress}/api/add/user`, JSON.stringify(user), (req) => { loginme(user) }).send();
 }
 
 function saveMemoScore(seconds, comparisons) {
@@ -41,53 +136,13 @@ function saveMemoScore(seconds, comparisons) {
     'comparisons': comparisons
   };
 
-  let request = getRequestObject();
-
-  request.onreadystatechange = function () {
-    if (request.readyState == 4 ) {
-
-      if(request.status == 200) {
-        console.log('OK');
-      }
-
-      else if(request.status == 401) {
-        onSessionLost();
-      }
-
-      else {
-        alert(`Error ${request.status}: ${request.response}`)
-      }
-    }
-  }
-
-  request.open("POST", `${appAddress}/api/add/score/memo`, true);
-  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  request.setRequestHeader('Authorization', 'JWT ' + getAccessToken())
-  request.send(JSON.stringify(score));
+  let req = new RequestWithAuth("POST", `${appAddress}/api/add/score/memo`, JSON.stringify(score),
+    (req) => { console.log(req.response); });
+  req.send();
 }
 
 function getUserInfo() {
-  let request = getRequestObject();
-
-  request.onreadystatechange = function() {
-    if (request.readyState == 4) {
-      if(request.status == 200) {
-        console.log(request.response);
-      }
-
-      else if (request.status = 401) {
-        onSessionLost();
-      }
-
-      else {
-        alert(`Error ${request.status}: ${request.response}`);
-      }
-    }
-  }
-  request.open("GET", `${appAddress}/api/user/info`, true);
-  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  request.setRequestHeader('Authorization', 'JWT ' + getAccessToken())
-  request.send(null);
+  new RequestWithAuth("GET", `${appAddress}/api/user/info`, null, (req) => { console.log(req.response); }).send();
 }
 
 // function sendGetRequest(url, successCallback) {
