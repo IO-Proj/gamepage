09968bb48e28cf8f2f79ea5d09f89a8c684d86cd
kingapyrek <kinga147852@onet.pl>
2021-05-14 16:54:24 +0200

    new badges

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var webAddress = "https://io-proj.github.io/gamepage";
-// var webAddress = "http://localhost:8000";
+ //var webAddress = "http://localhost:8000";
 
 var apiAddress = "https://io-projekt-gamepage-api.herokuapp.com";
-// var apiAddress = "http://localhost:5000";
+ //var apiAddress = "http://localhost:5000";
