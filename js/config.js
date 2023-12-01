9bada7a32be4335057d4bb03c10138e098b7bbf3
nszczerba99 <nataliaszczerba00@gmail.com>
2023-12-01 17:25:45 +0100

    Change api address

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var webAddress = "https://io-proj.github.io/gamepage";
 //var webAddress = "http://localhost:8000";
 
-var apiAddress = "https://io-projekt-gamepage-api.herokuapp.com";
+var apiAddress = "https://e-glazing-406813.ey.r.appspot.com";
 //var apiAddress = "http://localhost:5000";
